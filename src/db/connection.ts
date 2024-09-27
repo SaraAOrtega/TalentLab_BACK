f4c8456d4c968d9b916442f77d0cbae61384155e
SaraAOrtega <149887883+SaraAOrtega@users.noreply.github.com>
2024-09-27 16:36:04 +0200

    produccion 2709

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,14 +1,25 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+// Cargar las variables de entorno desde el archivo .env
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Si estás en producción, usa la URL de Railway, si no, usa los parámetros locales
+const dbUrl = process.env.MYSQL_URL;
 
 const dbName = process.env.DB_NAME || 'talentlab_db';
 const dbUser = process.env.DB_USER || 'root';
 const dbPassword = process.env.DB_PASSWORD || '';
 const dbHost = process.env.DB_HOST || 'localhost';
 
-const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
-  host: dbHost,
-  dialect: "mysql",
-  // Puedes agregar más opciones aquí si es necesario
-});
+const sequelize = isProduction 
+  ? new Sequelize(dbUrl || '', { dialect: 'mysql', logging: false }) // Si es producción, usa la URL de la base de datos
+  : new Sequelize(dbName, dbUser, dbPassword, {
+      host: dbHost,
+      dialect: 'mysql',
+      logging: console.log,  // Solo para debug en local
+    });
 
 export default sequelize;
