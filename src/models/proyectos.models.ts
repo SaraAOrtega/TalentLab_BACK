de4eff8bf894107faf6fba16771145434c12ac14
SaraAOrtega <149887883+SaraAOrtega@users.noreply.github.com>
2024-10-14 12:26:58 +0200

    proyectos model ok

--- src/models/proyectos.models.ts
+++ src/models/proyectos.models.ts
@@ -1,29 +1,32 @@
-import { DataTypes, Model } from "sequelize";
+import { DataTypes, Model, InferAttributes, InferCreationAttributes, CreationOptional, ForeignKey, NonAttribute } from "sequelize";
 import db from '../db/connection';
+import User from '../models/user.models';  // Asegúrate de que la ruta sea correcta
 
+class Proyecto extends Model<InferAttributes<Proyecto>, InferCreationAttributes<Proyecto>> {
+  declare id_proyecto: CreationOptional<number>;
+  declare user_id: ForeignKey<User['id_user']>;
+  declare nombre_proyecto: string;
+  declare director_proyecto: string;
+  declare fecha_pdv: Date | null;
+  declare fecha_rodaje: Date | null;
+  declare lugar: string | null;
+  declare descripcion: string | null;
 
-class Proyecto extends Model {
-  public id_proyecto!: number;
-  public user_id!: number;
-  public nombre_proyecto!: string;
-  public director_proyecto!: string;
-  public fecha_pdv!: Date;
-  public fecha_rodaje!: Date;
-  public lugar!: string;
-  public descripcion!: string;
-
+  declare user?: NonAttribute<User>;
+  declare personajes?: NonAttribute<Proyecto[]>;
 
   static associate(models: any) {
     Proyecto.hasMany(models.Personaje, { 
-        foreignKey: 'proyecto_id', 
-        as: 'personajes' 
+      foreignKey: 'proyecto_id', 
+      as: 'personajes' 
     });
     Proyecto.belongsTo(models.User, { 
-        foreignKey: 'user_id', 
-        as: 'user' 
+      foreignKey: 'user_id', 
+      as: 'user' 
     });
+  }
 }
-}
+
 Proyecto.init({
   id_proyecto: {
     type: DataTypes.INTEGER,
@@ -34,7 +37,7 @@ Proyecto.init({
     type: DataTypes.INTEGER,
     allowNull: false,
     references: {
-      model: 'Users', 
+      model: 'users',  // Cambiado a minúsculas
       key: 'id_user'
     }
   },
@@ -50,21 +53,25 @@ Proyecto.init({
     allowNull: false
   },
   fecha_pdv: {
-    type: DataTypes.DATE
+    type: DataTypes.DATE,
+    allowNull: true
   },
   fecha_rodaje: {
-    type: DataTypes.DATE
+    type: DataTypes.DATE,
+    allowNull: true
   },
   lugar: {
-    type: DataTypes.TEXT
+    type: DataTypes.TEXT,
+    allowNull: true
   },
   descripcion: {
-    type: DataTypes.TEXT
+    type: DataTypes.TEXT,
+    allowNull: true
   }
 }, {
   sequelize: db,
   modelName: 'Proyecto',
-  tableName: 'Proyectos',
+  tableName: 'proyectos',  // Cambiado a minúsculas
   timestamps: false
 });
 
