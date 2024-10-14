0ac14c30ac3c8cc27a8541d507ec036129e10615
SaraAOrtega <149887883+SaraAOrtega@users.noreply.github.com>
2024-10-14 12:20:56 +0200

    arreglo modelo user

--- src/models/user.models.ts
+++ src/models/user.models.ts
@@ -1,36 +1,50 @@
-
-import { DataTypes, Model } from "sequelize";
+import { DataTypes, Model, InferAttributes, InferCreationAttributes, CreationOptional } from "sequelize";
 import sequelize from "../db/connection";
-import { UserAttributes, UserCreationAttributes } from '../interfaces/user.interface'; 
 
-const User = sequelize.define<Model<UserAttributes, UserCreationAttributes>>('User', {
-  id_user: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+  declare id_user: CreationOptional<number>;
+  declare username: string;
+  declare email: string;
+  declare password: string;
+  declare documento: string;
+  declare rol: string | null;
+}
+
+User.init(
+  {
+    id_user: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    documento: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    rol: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
   },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  documento: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  rol: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  
-});
+  {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users', // Esto es crucial: fuerza el nombre de la tabla a 'users'
+    timestamps: true // Asumiendo que quieres timestamps
+  }
+);
 
 export default User;
